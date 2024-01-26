c416cd95f151d12795679fcbf4627b6a82f5fe5f
Awadesh <36awadesh@gmail.com>
2024-01-26 16:09:47 +0530

    Fixed Compilation BUGS: ADDED Router

--- src/App.js
+++ src/App.js
@@ -1,59 +1,37 @@
-// App.js
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Counter } from './features/counter/Counter';
+import './App.css';
 import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home></Home>,
+  },
+  {
+    path: '/login',
+    element: <LoginPage></LoginPage>,
+  },
+  {
+    path: '/signup',
+    element: <SignupPage></SignupPage>,
+  },
+]);
+
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-        </Routes>
-      </Router>
+      <RouterProvider router={router} />
     </div>
   );
 }
 
 export default App;
-
-// import { Counter } from './features/counter/Counter';
-// import './App.css';
-// import Home from './pages/Home';
-// import LoginPage from './pages/LoginPage';
-// import SignupPage from './pages/SignupPage';
-
-// import {
-//   createBrowserRouter,
-//   RouterProvider,
-//   Route,
-//   Link,
-// } from 'react-router-dom';
-
-// const router = createBrowserRouter([
-//   {
-//     path: '/',
-//     element: <Home></Home>,
-//   },
-//   {
-//     path: '/login',
-//     element: <LoginPage></LoginPage>,
-//   },
-//   {
-//     path: '/signup',
-//     element: <SignupPage></SignupPage>,
-//   },
-// ]);
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <RouterProvider router={router} />
-//     </div>
-//   );
-// }
-
-// export default App;

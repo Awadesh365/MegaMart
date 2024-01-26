901cb49d338d3deb4eccbeff695d86107dedd146
Awadesh <36awadesh@gmail.com>
2024-01-26 16:03:16 +0530

    TEST

--- src/App.js
+++ src/App.js
@@ -19,3 +19,41 @@ function App() {
 }
 
 export default App;
+
+// import { Counter } from './features/counter/Counter';
+// import './App.css';
+// import Home from './pages/Home';
+// import LoginPage from './pages/LoginPage';
+// import SignupPage from './pages/SignupPage';
+
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+//   Route,
+//   Link,
+// } from 'react-router-dom';
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Home></Home>,
+//   },
+//   {
+//     path: '/login',
+//     element: <LoginPage></LoginPage>,
+//   },
+//   {
+//     path: '/signup',
+//     element: <SignupPage></SignupPage>,
+//   },
+// ]);
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <RouterProvider router={router} />
+//     </div>
+//   );
+// }
+
+// export default App;

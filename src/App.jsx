bb669042a7aa78882fce16addd4d901edc1c4020
Awadesh <36awadesh@gmail.com>
2024-01-26 15:05:50 +0530

    Update Auth and Brand Name

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
-import { Counter } from './features/counter/Counter';
-import './App.css';
-import Home from './pages/Home';
-import LoginPage from './pages/LoginPage';
-import SignupPage from './pages/SignupPage';
+import React from "react";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 
 import {
   createBrowserRouter,
   RouterProvider,
   Route,
   Link,
-} from 'react-router-dom';
+} from "react-router-dom";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Home></Home>,
   },
   {
-    path: '/login',
+    path: "/login",
     element: <LoginPage></LoginPage>,
   },
   {
-    path: '/signup',
+    path: "/signup",
     element: <SignupPage></SignupPage>,
   },
 ]);
@@ -34,3 +35,5 @@ function App() {
     </div>
   );
 }
+
+export default App;

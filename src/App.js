570f6e1a549c5760a0bf1b6676cef18351c24552
Awadesh <36awadesh@gmail.com>
2024-01-27 18:12:32 +0530

    TESTING

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
+import CartPage from './pages/CartPage';
 
 import {
   createBrowserRouter,
@@ -10,6 +11,7 @@ import {
   Route,
   Link,
 } from "react-router-dom";
+
 
 const router = createBrowserRouter([
   {
@@ -21,9 +23,14 @@ const router = createBrowserRouter([
     element: <LoginPage></LoginPage>,
   },
   {
-    path: "/signup",
-    element: <SignupPage></SignupPage>,
-  }
+    path: "signup",
+    element: <SignupPage></SignupPage>
+  },
+  // for testing
+  {
+    path: "cart",
+    element: <CartPage></CartPage>,
+  },
 ]);
 
 function App() {

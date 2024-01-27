75bd2ea2d7e976fa3b5bffda629665f7c2e2ade4
Awadesh <36awadesh@gmail.com>
2024-01-27 15:59:11 +0530

    Update CartSlice

--- src/features/Cart/CartSlice.js
+++ src/features/Cart/CartSlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { fetchCount } from './CartAPI';
+import { fetchCount } from './cartAPI';
 
 const initialState = {
   value: 0,

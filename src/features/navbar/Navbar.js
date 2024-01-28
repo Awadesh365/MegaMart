011e5e7982a9fdb77231c52c93a8da1d48bb21e1
Awadesh <36awadesh@gmail.com>
2024-01-28 20:06:43 +0530

    UPDATE Nav

--- src/features/navbar/Navbar.js
+++ src/features/navbar/Navbar.js
@@ -209,7 +209,6 @@ function NavBar({ children }) {
                     </Link>
                     <span className="inline-flex items-center rounded-md bg-red-50 mb-7 -ml-3 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
                       3
-                      3
                     </span>
                   </div>
                   <div className="mt-3 space-y-1 px-2">

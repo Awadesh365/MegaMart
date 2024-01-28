a74ee417d7c0cbfc2ae6f7a54ffa86ef3779ed6a
Awadesh <36awadesh@gmail.com>
2024-01-28 20:49:30 +0530

    Resolve Double Cart Problem in mobile View

--- src/features/navbar/Navbar.js
+++ src/features/navbar/Navbar.js
@@ -22,9 +22,11 @@ const userNavigation = [
   { name: 'Settings', href: '#' },
   { name: 'Sign out', href: '#' },
 ];
+
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
 }
+
 function NavBar({ children }) {
   return (
     <>
@@ -64,16 +66,6 @@ function NavBar({ children }) {
                   </div>
                   <div className="hidden md:block">
                     <div className="ml-4 flex items-center md:ml-6">
-                      <button
-                        type="button"
-                        className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                      >
-                        <span className="sr-only">View notifications</span>
-                        <ShoppingCartIcon
-                          className="h-6 w-6"
-                          aria-hidden="true"
-                        />
-                      </button>
                       <Link to="/cart">
                         <button
                           type="button"
@@ -89,6 +81,7 @@ function NavBar({ children }) {
                       <span className="inline-flex items-center rounded-md mb-7 -ml-3 bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
                         3
                       </span>
+
                       {/* Profile dropdown */}
                       <Menu as="div" className="relative ml-3">
                         <div>
@@ -150,6 +143,7 @@ function NavBar({ children }) {
                   </div>
                 </div>
               </div>
+
               <Disclosure.Panel className="md:hidden">
                 <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
                   {navigation.map((item) => (
@@ -186,16 +180,6 @@ function NavBar({ children }) {
                         {user.email}
                       </div>
                     </div>
-                    <button
-                      type="button"
-                      className="ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                    >
-                      <ShoppingCartIcon
-                        className="h-6 w-6"
-                        aria-hidden="true"
-                      />
-
-                    </button>
                     <Link to="/cart">
                       <button
                         type="button"
@@ -208,7 +192,6 @@ function NavBar({ children }) {
                       </button>
                     </Link>
                     <span className="inline-flex items-center rounded-md bg-red-50 mb-7 -ml-3 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
-                      3
                       3
                     </span>
                   </div>
@@ -229,10 +212,11 @@ function NavBar({ children }) {
             </>
           )}
         </Disclosure>
+
         <header className="bg-white shadow">
           <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
             <h1 className="text-3xl font-bold tracking-tight text-gray-900">
-              E-Commerce
+              MegaMart
             </h1>
           </div>
         </header>
@@ -245,4 +229,5 @@ function NavBar({ children }) {
     </>
   );
 }
+
 export default NavBar;

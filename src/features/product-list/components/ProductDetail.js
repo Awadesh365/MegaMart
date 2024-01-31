854a623e54f892a3ed84d2197e98bff694e9afe5
Awadesh <36awadesh@gmail.com>
2024-01-31 23:46:01 +0530

    TEST

--- src/features/product-list/components/ProductDetail.js
+++ src/features/product-list/components/ProductDetail.js
@@ -81,7 +81,7 @@ function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
 }
 
-export default function Example() {
+export default function ProductDetail() {
     const [selectedColor, setSelectedColor] = useState(product.colors[0])
     const [selectedSize, setSelectedSize] = useState(product.sizes[2])
 

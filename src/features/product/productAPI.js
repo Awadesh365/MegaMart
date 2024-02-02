17437fb802f5e83a9ae1b18b52f0cdf1278af011
Awadesh <36awadesh@gmail.com>
2024-02-02 22:57:20 +0530

    Add fetchProduct filter and Test respose

--- src/features/product/productAPI.js
+++ src/features/product/productAPI.js
@@ -1,8 +1,26 @@
-export function fetchCount(amount = 1) {
-  return new Promise(async (resolve) =>{
-    const response = await fetch('http://localhost:8080') 
+export function fetchAllProducts() {
+  return new Promise(async (resolve) => {
+    //TODO: we will not hard-code server URL here
+    const response = await fetch('http://localhost:8080/products')
     const data = await response.json()
-    resolve({data})
+    resolve({ data })
+  }
+  );
+}
+
+export function fetchProductsByFilters(filter) {
+  // filter = {"category":"smartphone"}
+  // TODO : on server we will support multi values
+  let queryString = '';
+  for (let key in filter) {
+    queryString += `${key}=${filter[key]}&`
+  }
+
+  return new Promise(async (resolve) => {
+    //TODO: we will not hard-code server URL here
+    const response = await fetch('http://localhost:8080/products?' + queryString)
+    const data = await response.json()
+    resolve({ data })
   }
   );
 }

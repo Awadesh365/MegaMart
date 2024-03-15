118c3edd357e3df8ac3aa78c8d7efad5a0924666
Awadesh <36awadesh@gmail.com>
2024-03-15 13:19:02 +0530

    Implement Logout and Forgot Password functionality

--- src/features/auth/authAPI.js
+++ src/features/auth/authAPI.js
@@ -31,3 +31,10 @@ export function checkUser(loginInfo) {
   });
 }
 
+export function signOut(userId) {
+  return new Promise(async (resolve) => {
+    // TODO: on server we will remove user session info
+    resolve({ data: 'success' });
+  });
+}
+
--- src/features/auth/components/Login.js
+++ src/features/auth/components/Login.js
@@ -78,12 +78,12 @@ export default function Login() {
                   Password
                 </label>
                 <div className="text-sm">
-                  <a
-                    href="#"
+                  <Link
+                    to="/forgot-password"
                     className="font-semibold text-indigo-600 hover:text-indigo-500"
                   >
                     Forgot password?
-                  </a>
+                  </Link>
                 </div>
               </div>
               <div className="mt-2">

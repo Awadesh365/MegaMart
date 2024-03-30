5d48fde144e50252ab6ae527524116403ea862cf
Awadesh <36awadesh@gmail.com>
2024-03-30 16:53:15 +0530

    Setup and execute server functionality.

--- client/src/components/authButton/Signin.jsx
+++ client/src/components/authButton/Signin.jsx
@@ -15,7 +15,9 @@ export default function Signin() {
           <FaMoon />
         </Button>
         <Link to="/sign-in">
-          <Button gradientDuoTone="purpleToBlue">Sign In</Button>
+          <Button gradientDuoTone="purpleToBlue" outline>
+            Sign In
+          </Button>
         </Link>
         <Navbar.Toggle />
       </div>
